e56fd85cd1fcc72e2523c13dc1a8707e3c79c587
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2023-07-04 20:55:25 -0300

    ClimaOpenWeather
    codando um ClimaOpenWeather com API

---
+++ ClimaOpenWeatherAPI/js.js
@@ -0,0 +1,16 @@
+// const primeiraFuncao = () => {
+//     setTimeout(() => {
+//         console.log("Esperou por aqui");
+//     },2000);
+// }
+
+//  const segundaFuncao = async() => {
+//     console.log("Começou");
+//     await primeiraFuncao();
+//     console.log("Terminou")
+// }
+
+// segundaFuncao();
+"use strict"; 
+
+console.log(nome)
